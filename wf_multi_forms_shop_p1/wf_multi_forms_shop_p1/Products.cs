253bbd738ab433a1fe1f77d9b5b6765748d30c65
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-06-08 16:12:20 +0300

    create class "Products"

--- wf_multi_forms_shop_p1/wf_multi_forms_shop_p1/Products.cs
+++ wf_multi_forms_shop_p1/wf_multi_forms_shop_p1/Products.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace wf_multi_forms_shop_p1
 {
-    internal class Products
+    public class Product
     {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Country { get; set; }
+        public int DiscountPercent { get; set; }
+        public Product(string name, decimal price, int quantity, string country, int discount)
+        {
+            Name = name; Price = price; Quantity = quantity; Country = country; DiscountPercent = discount;
+        }
+        public Product()
+        {
+            Name = string.Empty;
+            Price = default;
+            Quantity = default;
+            Country = string.Empty;
+            DiscountPercent = default;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: {Price}UAH, Quantity: {Quantity}, Country: {Country}, Discount: {DiscountPercent}";
+        }
     }
 }
