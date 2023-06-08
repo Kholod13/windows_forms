dd0fac6df83d8c6cc20517594d2b62886d03d923
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-06-08 16:11:38 +0300

    create files

---
+++ wf_multi_forms_shop_p1/wf_multi_forms_shop_p1/Products.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wf_multi_forms_shop_p1
+{
+    internal class Products
+    {
+    }
+}
