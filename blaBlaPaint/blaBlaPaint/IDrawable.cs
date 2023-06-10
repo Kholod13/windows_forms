ebc540f17f71091920cf7007012c98b94c2b6cf8
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-06-10 23:57:55 +0300

    create all classes

---
+++ blaBlaPaint/blaBlaPaint/IDrawable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blaBlaPaint
+{
+    internal interface IDrawable
+    {
+        void Drawing(Graphics graphics);
+        void StartDrawing(Point startPoint);
+        void ContinueDrawing(Point nextPoint);
+    }
+}
