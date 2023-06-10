d6957decacdf651cf1be535c050b7b61d8b3a3e6
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-06-10 23:37:45 +0300

    finish project

---
+++ wf_finder_full/wf_finder_full/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace wf_finder_full
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonOpenRoot = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButtonMinus = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonPlus = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonNewDirectory = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBoxNewDirectory = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButtonRenameDirectory = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBoxRenameDirectory = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButtonRenameFile = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBoxRenameFile = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButtonDeleteFile = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonDublicate = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonShowHidden = new System.Windows.Forms.ToolStripButton();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.toolStripComboBoxView = new System.Windows.Forms.ToolStripComboBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonOpenRoot,
+            this.toolStripLabel1,
+            this.toolStripComboBoxView,
+            this.toolStripButtonMinus,
+            this.toolStripButtonPlus,
+            this.toolStripButtonNewDirectory,
+            this.toolStripTextBoxNewDirectory,
+            this.toolStripButtonRenameDirectory,
+            this.toolStripTextBoxRenameDirectory,
+            this.toolStripButtonRenameFile,
+            this.toolStripTextBoxRenameFile,
+            this.toolStripButtonDeleteFile,
+            this.toolStripButtonDublicate,
+            this.toolStripButtonShowHidden});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1078, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonOpenRoot
+            // 
+            this.toolStripButtonOpenRoot.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonOpenRoot.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonOpenRoot.Image")));
+            this.toolStripButtonOpenRoot.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonOpenRoot.Name = "toolStripButtonOpenRoot";
+            this.toolStripButtonOpenRoot.Size = new System.Drawing.Size(68, 22);
+            this.toolStripButtonOpenRoot.Text = "Open Root";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(35, 22);
+            this.toolStripLabel1.Text = "View:";
+            // 
+            // toolStripButtonMinus
+            // 
+            this.toolStripButtonMinus.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonMinus.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonMinus.Image")));
+            this.toolStripButtonMinus.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonMinus.Name = "toolStripButtonMinus";
+            this.toolStripButtonMinus.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonMinus.Text = "toolStripButton2";
+            // 
+            // toolStripButtonPlus
+            // 
+            this.toolStripButtonPlus.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonPlus.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonPlus.Image")));
+            this.toolStripButtonPlus.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonPlus.Name = "toolStripButtonPlus";
+            this.toolStripButtonPlus.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonPlus.Text = "toolStripButton3";
+            // 
+            // toolStripButtonNewDirectory
+            // 
+            this.toolStripButtonNewDirectory.Name = "toolStripButtonNewDirectory";
+            this.toolStripButtonNewDirectory.Size = new System.Drawing.Size(84, 22);
+            this.toolStripButtonNewDirectory.Text = "New directory:";
+            // 
+            // toolStripTextBoxNewDirectory
+            // 
+            this.toolStripTextBoxNewDirectory.Name = "toolStripTextBoxNewDirectory";
+            this.toolStripTextBoxNewDirectory.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripButtonRenameDirectory
+            // 
+            this.toolStripButtonRenameDirectory.Name = "toolStripButtonRenameDirectory";
+            this.toolStripButtonRenameDirectory.Size = new System.Drawing.Size(103, 22);
+            this.toolStripButtonRenameDirectory.Text = "Rename directory:";
+            // 
+            // toolStripTextBoxRenameDirectory
+            // 
+            this.toolStripTextBoxRenameDirectory.Name = "toolStripTextBoxRenameDirectory";
+            this.toolStripTextBoxRenameDirectory.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripButtonRenameFile
+            // 
+            this.toolStripButtonRenameFile.Name = "toolStripButtonRenameFile";
+            this.toolStripButtonRenameFile.Size = new System.Drawing.Size(72, 22);
+            this.toolStripButtonRenameFile.Text = "Rename file:";
+            // 
+            // toolStripTextBoxRenameFile
+            // 
+            this.toolStripTextBoxRenameFile.Name = "toolStripTextBoxRenameFile";
+            this.toolStripTextBoxRenameFile.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripButtonDeleteFile
+            // 
+            this.toolStripButtonDeleteFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonDeleteFile.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonDeleteFile.Image")));
+            this.toolStripButtonDeleteFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonDeleteFile.Name = "toolStripButtonDeleteFile";
+            this.toolStripButtonDeleteFile.Size = new System.Drawing.Size(63, 22);
+            this.toolStripButtonDeleteFile.Text = "Delete file";
+            // 
+            // toolStripButtonDublicate
+            // 
+            this.toolStripButtonDublicate.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonDublicate.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonDublicate.Image")));
+            this.toolStripButtonDublicate.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonDublicate.Name = "toolStripButtonDublicate";
+            this.toolStripButtonDublicate.Size = new System.Drawing.Size(61, 22);
+            this.toolStripButtonDublicate.Text = "Dublicate";
+            // 
+            // toolStripButtonShowHidden
+            // 
+            this.toolStripButtonShowHidden.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonShowHidden.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonShowHidden.Image")));
+            this.toolStripButtonShowHidden.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonShowHidden.Name = "toolStripButtonShowHidden";
+            this.toolStripButtonShowHidden.Size = new System.Drawing.Size(83, 22);
+            this.toolStripButtonShowHidden.Text = "Show hedden";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 41);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(471, 397);
+            this.treeView1.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(511, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(547, 397);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // toolStripComboBoxView
+            // 
+            this.toolStripComboBoxView.Name = "toolStripComboBoxView";
+            this.toolStripComboBoxView.Size = new System.Drawing.Size(121, 25);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButtonOpenRoot;
+        private ToolStripLabel toolStripLabel1;
+        private ToolStripButton toolStripButtonMinus;
+        private ToolStripButton toolStripButtonPlus;
+        private ToolStripLabel toolStripButtonNewDirectory;
+        private ToolStripTextBox toolStripTextBoxNewDirectory;
+        private ToolStripLabel toolStripButtonRenameDirectory;
+        private ToolStripTextBox toolStripTextBoxRenameDirectory;
+        private ToolStripLabel toolStripButtonRenameFile;
+        private ToolStripTextBox toolStripTextBoxRenameFile;
+        private ToolStripButton toolStripButtonDeleteFile;
+        private ToolStripButton toolStripButtonDublicate;
+        private ToolStripButton toolStripButtonShowHidden;
+        private TreeView treeView1;
+        private ListView listView1;
+        private ToolStripComboBox toolStripComboBoxView;
+        private ImageList imageList1;
+    }
+}
---
+++ wf_finder_full/wf_finder_full/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System.Windows.Forms;
+
+namespace wf_finder_full
+{
+    public partial class Form1 : Form
+    {
+        private const int preloadLevel = 2;
+        private string currentPath;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            PreloadDirectories(desktopPath);
+            currentPath = desktopPath;
+        }
+
+        private void PreloadDirectories(string root)
+        {
+            treeView1.Nodes.Clear();
+            LoadDirectoriesRecursive(root, treeView1.Nodes, preloadLevel);
+        }
+
+        private void LoadDirectoriesRecursive(string root, TreeNodeCollection collection, int level)
+        {
+            DirectoryInfo rootDir = new(root);
+
+            foreach (var dir in rootDir.GetDirectories()) 
+            {
+               
+                if (toolStripButtonShowHidden.Checked == false && (dir.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                {
+                    continue;
+                }
+
+                TreeNode node = new(dir.Name);
+
+                node.Tag = dir.FullName;
+
+                collection.Add(node);
+
+                try
+                {
+
+                    if (level > 1 && dir.GetDirectories().Any())
+                    {
+                        LoadDirectoriesRecursive(dir.FullName, node.Nodes, level - 1);
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.ImageIndex = 1;
+            foreach (TreeNode node in e.Node.Nodes)
+            {
+
+                string path = node.Tag.ToString();
+                LoadDirectoriesRecursive(path, node.Nodes, 1);
+            }
+        }
+
+        private void toolStripButtonOpenRoot_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                currentPath = dialog.SelectedPath;
+                PreloadDirectories(dialog.SelectedPath);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string path = e.Node.Tag.ToString();
+            DirectoryInfo dir = new(path);
+            listView1.Clear();
+
+            listView1.Columns.Add("Name", 200);
+            listView1.Columns.Add("Size, kb", 100);
+            listView1.Columns.Add("Last Modified", 150);
+            foreach (var file in dir.GetFiles())
+            {
+                
+                if (toolStripButtonShowHidden.Checked == false && (file.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                {
+                    continue;
+                }
+
+                ListViewItem item = new ListViewItem(file.Name, 0);
+                item.Tag = file.FullName;
+                item.SubItems.Add((file.Length / 1024).ToString());
+                item.SubItems.Add(file.LastWriteTime.ToString());
+
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void treeView1_AfterCollapse(object sender, TreeViewEventArgs e)
+        {
+            e.Node.ImageIndex = 0;
+        }
+
+        private void toolStripButtonNewDirectory_Click(object sender, EventArgs e)
+        {
+            
+            string directoryName = toolStripTextBoxNewDirectory.Text;
+
+            if (treeView1.SelectedNode.Nodes.ContainsKey(directoryName))
+            {
+                MessageBox.Show("Директорія з таким ім'ям вже існує.");
+                return;
+            }
+
+           
+            TreeNode newDirectoryNode = new TreeNode(directoryName);
+            newDirectoryNode.Tag = Path.Combine((string)treeView1.SelectedNode.Tag, directoryName);
+            newDirectoryNode.Name = directoryName;
+
+            treeView1.SelectedNode.Nodes.Add(newDirectoryNode);
+
+         
+            string path = Path.Combine((string)treeView1.SelectedNode.Tag, directoryName);
+            System.IO.Directory.CreateDirectory(path);
+        }
+
+        private void toolStripButtonRenameDirectory_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = treeView1.SelectedNode;
+            string newName = toolStripTextBoxRenameDirectory.Text;
+
+            if (selectedNode != null)
+            {
+                
+                if (string.IsNullOrWhiteSpace(newName) || newName.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+                {
+                    MessageBox.Show("Недопустиме ім'я папки.");
+                    return;
+                }
+
+               
+                if (selectedNode.Parent.Nodes.ContainsKey(newName))
+                {
+                    MessageBox.Show("Папка з таким ім'ям вже існує.");
+                    return;
+                }
+
+                string oldPath = (string)treeView1.SelectedNode.Tag;
+                string newPath = Path.Combine((string)treeView1.SelectedNode.Parent.Tag, newName);
+                System.IO.Directory.Move(oldPath, newPath);
+
+
+                selectedNode.Text = newName;
+                selectedNode.Tag = Path.Combine((string)treeView1.SelectedNode.Parent.Tag, newName);
+            }
+        }
+
+        private void toolStripButtonRenameFile_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedFile = listView1.SelectedItems[0];
+                string currentFilePath = selectedFile.Tag.ToString(); 
+               
+                string newFileName = toolStripTextBoxRenameFile.Text;
+
+                if (!string.IsNullOrEmpty(newFileName))
+                {
+                    try
+                    {
+                        string directoryPath = Path.GetDirectoryName(currentFilePath);
+                        string newFilePath = Path.Combine(directoryPath, newFileName);
+
+                        File.Move(currentFilePath, newFilePath);
+
+
+                        selectedFile.Text = newFileName;
+                        selectedFile.Tag = newFilePath;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Помилка при перейменуванні файлу: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private void toolStripButtonDeleteFile_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedFile = listView1.SelectedItems[0];
+                string filePath = selectedFile.Tag.ToString(); // Передбачається, що шлях до файлу зберігається в властивості Tag елементу ListViewItem
+
+                try
+                {
+                    File.Delete(filePath);
+
+                    listView1.Items.Remove(selectedFile);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка при видаленні файлу: {ex.Message}");
+                }
+            }
+        }
+
+        private void toolStripButtonDublicate_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedFile = listView1.SelectedItems[0];
+                string filePath = selectedFile.Tag.ToString(); 
+
+                try
+                {
+                    string directoryPath = Path.GetDirectoryName(filePath);
+                    string uniqueFileName = GenerateUniqueFileName(filePath);
+                    string newFilePath = Path.Combine(directoryPath, uniqueFileName);
+
+                    File.Copy(filePath, newFilePath);
+
+                    ListViewItem duplicateFile = new ListViewItem(Path.GetFileName(newFilePath));
+                    duplicateFile.Tag = newFilePath;
+                    duplicateFile.ImageIndex = 0; 
+                    listView1.SmallImageList = imageList1;
+                    listView1.Items.Add(duplicateFile);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка при створенні дублікату файлу: {ex.Message}");
+                }
+            }
+        }
+        private string GenerateUniqueFileName(string filePath)
+        {
+            string directoryPath = Path.GetDirectoryName(filePath);
+            string fileName = Path.GetFileNameWithoutExtension(filePath);
+            string fileExtension = Path.GetExtension(filePath);
+            string uniqueFileName = $"{fileName}_copy{fileExtension}";
+            int counter = 1;
+
+            while (File.Exists(Path.Combine(directoryPath, uniqueFileName)))
+            {
+                uniqueFileName = $"{fileName}_copy{counter}{fileExtension}";
+                counter++;
+            }
+
+            return uniqueFileName;
+        }
+
+        private void toolStripButtonShowHidden_Click(object sender, EventArgs e)
+        {
+            if (toolStripButtonShowHidden.Checked == true)
+            {
+                toolStripButtonShowHidden.Checked = false;
+                toolStripButtonShowHidden.Text = "Hide Hidden";
+            }
+            else
+            {
+                toolStripButtonShowHidden.Checked = true;
+                toolStripButtonShowHidden.Text = "Show Hidden";
+            }
+
+            listView1.Items.Clear();
+            treeView1.Nodes.Clear();
+            LoadDirectoriesRecursive(currentPath, treeView1.Nodes, 2);
+        }
+
+        private void toolStripComboBoxView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (toolStripComboBoxView.SelectedIndex == 0)
+                listView1.View = View.Tile;
+            else if (toolStripComboBoxView.SelectedIndex == 1)
+                listView1.View = View.Details;
+            else if (toolStripComboBoxView.SelectedIndex == 2)
+                listView1.View = View.LargeIcon;
+            else if (toolStripComboBoxView.SelectedIndex == 3)
+                listView1.View = View.SmallIcon;
+            else if (toolStripComboBoxView.SelectedIndex == 4)
+                listView1.View = View.List;
+
+            if (toolStripComboBoxView.SelectedIndex == 0)
+            {
+                toolStripButtonMinus.Visible = true;
+                toolStripButtonPlus.Visible = true;
+            }
+            else
+            {
+                toolStripButtonMinus.Visible = false;
+                toolStripButtonPlus.Visible = false;
+            }
+        }
+
+        private void toolStripButtonMinus_Click(object sender, EventArgs e)
+        {
+            listView1.TileSize = new Size(listView1.TileSize.Width - 10, listView1.TileSize.Height - 10);
+        }
+
+        private void toolStripButtonPlus_Click(object sender, EventArgs e)
+        {
+            listView1.TileSize = new Size(listView1.TileSize.Width + 10, listView1.TileSize.Height + 10);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
